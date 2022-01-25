5827bd0ca480f71ff7fef2dc41738128951b63db
Collin Hubbard <chubbard9@gatech.edu>
2022-01-24 21:56:48 -0500

    codacy

--- root/frontend/public/scripts/gallery-script.js
+++ root/frontend/public/scripts/gallery-script.js
@@ -19,7 +19,7 @@ $("document").ready(function() {
 
     // Modify the slidshow settings here
     $(".slideshow").slick({
-        prevArrow: `<i class="fas fa-arrow-left prevArrowBtn"></i>`,
-        nextArrow: `<i class="fas fa-arrow-right nextArrowBtn"></i>`
+        prevArrow: "<i class='fas fa-arrow-left prevArrowBtn'></i>",
+        nextArrow: "<i class=fas fa-arrow-right nextArrowBtn'></i>"
     });
 });
