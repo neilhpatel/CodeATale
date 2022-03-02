84915e1646af9353c6ed891ba471bf459c9a0882
neilhpatel <neilpatel612@icloud.com>
2022-03-02 13:29:56 -0500

    Resolved merge conflict in main.

--- root/backend/src/DefParser.py
+++ root/backend/src/DefParser.py
@@ -1,3 +1,4 @@
+from xml.etree.ElementInclude import FatalIncludeError
 import docx2txt
 import os
 import collections
@@ -16,7 +17,6 @@ firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-
 class DefParser():
 
     def __init__(self, filePath):
@@ -29,6 +29,8 @@ class DefParser():
         textInd = 0
         # Get rid of tab characters in parsed document
         self.text = self.text.replace("\t", "")
+        
+
         # Iterate through the text one character at a time
         while textInd < len(self.text):
             letter = self.text[textInd]
@@ -51,7 +53,7 @@ class DefParser():
                     lineInd += 1
                 word = word.lower()
 
-                # increment lineInd to the next non-space character
+                #increment lineInd to the next non-space character
                 while line[lineInd] == ' ':
                     lineInd += 1
                 # If the above loop ended at a newline character, current word is a sight word and no more parsing is
@@ -76,7 +78,7 @@ class DefParser():
                             else:
                                 currSubword += line[lineInd]
                                 lineInd += 1
-                        # make lineInd point to character right after ')'
+                        #make lineInd point to character right after ')'
                         lineInd += 1
                         # make lineInd point to the next non-space character, should either be '/n' if sight word
                         # and '=' if not
@@ -88,10 +90,9 @@ class DefParser():
                     definition = line[lineInd+2:len(line)-1].lower()
                     if ("[" in definition):
                         start = definition.index("[")
-                        blockedQuizOptions = definition[start +
-                                                        4:len(definition)-1]
+                        blockedQuizOptions = definition[start+4:len(definition)-1]
                         definition = definition[:start]
-                    # if not sightWord:
+                    #if not sightWord:
                      #   definition = line[lineInd + 2:len(line) - 1]
 
                 # if a word has no definition, it is a sight word
@@ -108,27 +109,29 @@ class DefParser():
                 # populate database; no need to run this every time so it can be commented out until needed
                 collection_ref = db.collection(word[0])
                 doc_ref = collection_ref.document(word)
-                doc_ref.set({
-                    'is_sight_word': sightWord,
-                    'is_child_word': False,
-                    'parent_word': word,
-                    'definition': definition,
-                    'derivative_words': childWords,
-                    'block_from_quiz': blockedQuizOptions,
-                })
+                # doc_ref.set({
+                #     'is_sight_word': sightWord,
+                #     'is_child_word': False,
+                #     'parent_word': word,
+                #     'definition': definition,
+                #     'derivative_words': childWords,
+                #     'block_from_quiz': blockedQuizOptions,
+                # })
+                #
+                # for child in childWords:
+                #     collection_ref = db.collection(child[0])
+                #     doc_ref = collection_ref.document(child)
+                #     doc_ref.set({
+                #         'is_sight_word': False,
+                #         'is_child_word': True,
+                #         'parent_word': word,
+                #         'definition': "",
+                #         'derivative_words': "",
+                #         'block_from_quiz': ""
+                #     })
 
-                for child in childWords:
-                    collection_ref = db.collection(child[0])
-                    doc_ref = collection_ref.document(child)
-                    doc_ref.set({
-                        'is_sight_word': False,
-                        'is_child_word': True,
-                        'parent_word': word,
-                        'definition': "",
-                        'derivative_words': "",
-                        'block_from_quiz': ""
-                    })
-                # print to test data values
+
+                # # print to test data values              
                 # print("Word: " + word)
                 # print("Is Child? False,\t\tParent Word: \"\"")
                 # print("Definition: " + definition)
@@ -154,24 +157,23 @@ class DefParser():
                 # skip newline character in between entries
                 textInd += 1
             textInd += 1
+        
          # adding last word
         last_line = self.text[startInd:]
         last_word = last_line[0:last_line.index("=")].lower()
-        last_definition = last_line[last_line.index(
-            "=")+2:last_line.index("[")].strip().lower()
-        last_blocked_quiz_options = last_line[last_line.index(
-            "graze"):-1].lower()
+        last_definition = last_line[last_line.index("=")+2:last_line.index("[")].strip().lower()
+        last_blocked_quiz_options = last_line[last_line.index("graze"):-1].lower()
 
-        collection_ref = db.collection(last_word[0])
-        doc_ref = collection_ref.document(last_word)
-        doc_ref.set({
-            'is_sight_word': False,
-            'is_child_word': False,
-            'parent_word': last_word,
-            'definition': last_definition,
-            'derivative_words': [],
-            'block_from_quiz': last_blocked_quiz_options,
-        })
+        # collection_ref = db.collection(last_word[0])
+        # doc_ref = collection_ref.document(last_word)
+        # doc_ref.set({
+        #     'is_sight_word': False,
+        #     'is_child_word': False,
+        #     'parent_word': last_word,
+        #     'definition': last_definition,
+        #     'derivative_words': [],
+        #     'block_from_quiz': last_blocked_quiz_options,
+        # })
 
 
 def main():
@@ -185,7 +187,6 @@ def main():
     srcPath = os.path.join(docPath, "definitions.docx")
     defParser = DefParser(srcPath)
     defParser.parseWordDocumentText()
-
 
 if __name__ == '__main__':
     main()
